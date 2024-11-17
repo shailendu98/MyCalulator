6dd1baff42837b93e772a0a2ac4a7d20eafc9e1a
shailendu98 <kumarshailendu98@gmail.com>
2024-11-17 19:46:24 +0530

    Add color according result

--- MyCalulator/ViewController.swift
+++ MyCalulator/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var calculateNumber: UILabel!
     @IBOutlet weak var calculateResult: UILabel!
+    @IBOutlet weak var resultView: UIView!
     
     private var working : String = " "
     private var viewModel: ViewModel = ViewModel()
@@ -43,8 +44,35 @@ class ViewController: UIViewController {
     }
     
     @IBAction func equalBTN(_ sender: UIButton){
-        calculateResult.text = viewModel.calculateResultString(working: working)
+        let value = viewModel.calculateResultString(working: working)
+        let result = Double(value) ?? 0
+        let newResult = result/1000
+        
+        if newResult >= 0 && newResult <= 1 { // 0 -- 1000 red color
+            let redValue = result/1000
+            resultView.backgroundColor = UIColor.red.withAlphaComponent(redValue)
+        } else if newResult >= 1 && newResult <= 2 { // 1000 -- 2000 color green
+            let greenValue = result/2000
+            resultView.backgroundColor = UIColor.green.withAlphaComponent(greenValue)
+        } else if newResult >= 2 && newResult <= 4 { // 2000 -- 4000 color blue
+            let greenValue = result/4000
+            resultView.backgroundColor = UIColor.blue.withAlphaComponent(greenValue)
+        } else if newResult >= 4 && newResult <= 8 { // 4000 -- 8000 color yellow
+            let greenValue = result/8000
+            resultView.backgroundColor = UIColor.yellow.withAlphaComponent(greenValue)
+        }else{
+            resultView.backgroundColor = UIColor.black.withAlphaComponent(newResult)
+        }
+        calculateResult.text = value
+        
+        //0-1000 // red
+        //1000-2000 // Green
+        //2000-4000// blue
+        //4000-8000 // black
+        
     }
+    
+    
     
     @IBAction private func didSymbolPress(_ sender: UIButton) {
         addToWorking(value: sender.titleLabel?.text ?? "")
